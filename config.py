81883777fab7ece68bc121503482bb9955929ed8
Faruk Cinemre <faruk_cinemre21@erdogan.edu.tr>
2025-08-03 14:43:40 +0300

    config eklendi

---
+++ config.py
@@ -0,0 +1,8 @@
+# Veritabanı ve uygulama konfigürasyonları
+DB_HOST = "localhost"
+DB_USER = "root"
+DB_PASSWORD = "Trabzonspor61."
+DB_NAME = "health_db"
+DEBUG = True
+HOST = "0.0.0.0"
+PORT = 5001
