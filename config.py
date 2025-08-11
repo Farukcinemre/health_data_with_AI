6dba0687a5a2fe0af0b50315538bd16c2bfd1924
Faruk Cinemre <faruk_cinemre21@erdogan.edu.tr>
2025-08-12 02:07:27 +0300

    config edited

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 DB_HOST = "localhost"  # Yerel MySQL sunucusu için 'localhost' veya '127.0.0.1'
 DB_USER = "root"       # MySQL kullanıcı adınız
-DB_PASSWORD = "Trabzonspor61."  # MySQL şifreniz
-DB_NAME = "health_db"  # Veritabanı adı
+DB_PASSWORD = "<your_password>"  # MySQL şifreniz
+DB_NAME = "<your_db_name>"  # Veritabanı adı
 DEBUG = True           # Geliştirme için True, üretim için False
 HOST = "0.0.0.0"       # Flask sunucusu için host
 PORT = 5000            # Flask sunucusu için port
